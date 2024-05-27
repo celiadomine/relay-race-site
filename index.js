6b18b956b5faa591517e048b38c3ab311103a575
Célia Dominé <celia.domine@meteoblue.com>
2024-05-27 15:16:10 +0200

    quick fixes (favicon, font, sizes)

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const PORT = 3003;
+
 const express = require("express");
 
 const app = express();
 
-app.use(express.static("./"))
+app.use(express.static("./"));
 
-app.listen(3000, () => {});
+app.listen(PORT, () => {
+    console.log(`listening ${PORT}`);
+});
