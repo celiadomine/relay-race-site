9b607b461ee8be451a609a7d2e261127865ea0fd
Célia Dominé <celia.domine@meteoblue.com>
2024-06-05 23:11:06 +0200

    FIXED NAVBAR FOR MOBILE YAYY

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //that it scrolls to top when you click on a label
-document.addEventListener('DOMContentLoaded', () => {
-    const navLabels = document.querySelectorAll('nav label');
+if (window.innerWidth >= 800) {
+    document.addEventListener('DOMContentLoaded', () => {
+        const navLabels = document.querySelectorAll('nav label');
 
-    navLabels.forEach(label => {
-        label.addEventListener('click', () => {
-            window.scrollTo(0, 0);
+        navLabels.forEach(label => {
+            label.addEventListener('click', () => {
+                window.scrollTo(0, 0);
+            });
         });
     });
-});
+}
